d5eb3f02ef76062ba8d1ef9e4c3fb54f9cde9bdd
not_loki <brianburke1030@live.com>
2022-08-30 04:58:29 -0500

    Add files via upload

--- snap-downloader.py
+++ snap-downloader.py
@@ -1,8 +1,6 @@
-from platform import architecture
 from sys import argv
 from requests import get
 import json
-from dl import dlfile
 
 try:
   snap_name = argv[1]
@@ -12,7 +10,31 @@ except:
 if len(argv) < 3:
   arch = 'amd64'
 else:
-  arch = argv[1]
+  arch = argv[2]
+
+def dlfile(url: str, save_path: str) -> None:
+  
+  dlreq = get(url, stream= True)
+  local_filename = url.split('/')[-1]
+  path = save_path + local_filename
+  accum: int = 0
+  if dlreq.status_code == 200:
+    with open (path, 'wb') as f:
+      for chunk in dlreq.iter_content(chunk_size=1024*1024):
+        if chunk:
+          accum += len(chunk)
+          
+          print('Downloaded: {}'.format(accum),sep='\x1b[2K\r')
+          f.write(chunk)
+    
+    f.close()
+    dlreq.close()
+    
+    #return dict({'bytes':accum, 'file':local_filename})    
+  else:
+    dlreq.close()
+    #return dict({'bytes':0, 'file':'failed'})
+
 
 snap_api_uri = 'https://api.snapcraft.io/v2/snaps/info/' + snap_name
 outfile_name = snap_name + '_' + arch + '.snap'
